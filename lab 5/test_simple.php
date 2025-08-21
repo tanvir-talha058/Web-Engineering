dd1f487923a0a9abfc8bb619968f0ed4b0387671
Tanvir <tanvirahmed123000@gmail.com>
2025-08-21 19:29:21 +0600

    test file

---
+++ lab 5/test_simple.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>PHP Server is Working!</h1>";
+echo "<p>Current time: " . date('Y-m-d H:i:s') . "</p>";
+echo "<p>PHP Version: " . phpversion() . "</p>";
+echo "<a href='index.php'>Go to Biodata System</a>";
+?>
